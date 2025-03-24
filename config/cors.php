1c66a852566691ca193479100058a11619cce6b2
Nguyễn Quốc Việt <88937231+nqv1710@users.noreply.github.com>
2025-03-24 10:08:34 +0700

    push

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://khodien.ringnet.vn:5174'], // Chỉ chấp nhận domain này
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
